9fb35b288d262a5a7cea7b27bb3a0cb6d4dda68a
Masao Mochizuki <mochizuki.masao@classmethod.jp>
2014-05-07 15:59:40 +0900

    added test

--- spec/aws/profile_parser_spec.rb
+++ spec/aws/profile_parser_spec.rb
@@ -1,11 +1,33 @@
 require 'spec_helper'
 
-describe Aws::ProfileParser do
+describe AWS::ProfileParser do
+
   it 'has a version number' do
-    expect(Aws::ProfileParser::VERSION).not_to be nil
+    expect(AWS::ProfileParser::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+end
+
+describe "ProfileParser" do
+
+  before :all do
+    ENV['AWS_CONFIG_FILE'] = File.dirname(__FILE__) + "/../data/awsconfig.sample"
+    @parser = AWS::ProfileParser.new
   end
+
+  it 'should have the default config file' do
+    expect(File.exists?(ENV['AWS_CONFIG_FILE'])).to be_true
+  end
+
+  it 'should return Hash' do
+    expect(@parser.get('default')).to be_a(Hash)
+  end
+
+  it 'should have keys' do
+    option = @parser.get('default')
+    expect(option).to have_key(:access_key_id)
+    expect(option).to have_key(:secret_access_key)
+    expect(option).to have_key(:region)
+  end
+
 end
